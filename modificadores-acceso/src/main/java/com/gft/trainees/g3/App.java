9c508db1f32acc8c5984415e1608a693b4001f8e
Luis Zepeda <zepedagonzalezluis@gmail.com>
2020-03-25 07:33:44 -0600

    Practicas Java

---
+++ modificadores-acceso/src/main/java/com/gft/trainees/g3/App.java
@@ -0,0 +1,13 @@
+package com.gft.trainees.g3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
